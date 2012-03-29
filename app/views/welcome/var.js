04356ce822ff45b5c816e14af57f9c1704a2822e
Trey Morris <tjm5114@gmail.com>
2012-03-29 05:18:54 -0400

    Dynamic Layout Test 1

---
+++ app/views/welcome/var.js
@@ -0,0 +1,10 @@
+		$critIs = "1";
+		$critOf = "3";
+    	$accompIs = "1";
+    	$accompOf = "4";
+    	$pointsIs = "1";
+    	$pointsOf = "10";
+    	$desIs = "1";
+    	$desOf = "4";
+    	
+    	
